92fb9b3d55e1a056637f058dd0608832bf9c8282
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-11-14 16:06:08 -0300

    adicionando assets e iniciando customizacao header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,21 @@
+import { NavLink } from 'react-router-dom'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { HeaderContainer } from './styles'
+import logoCoffeeDelivery from '../../assets/logo-coffee-delivery.svg'
+
 export function Header() {
     return (
-        <h1>Header</h1>
+        <HeaderContainer>
+            <img src={logoCoffeeDelivery} alt="Copo de café com um foguete do lado das palavras Coffee Delivery" />
+            <nav>
+                <NavLink to='/'>
+                    <MapPin weight='fill' size={22} />
+                    <p>Araxá, MG</p>
+                </NavLink>
+                <NavLink to='/cart'>
+                    <ShoppingCart weight='fill' size={22} />
+                </NavLink>
+            </nav>
+        </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,1 +1,29 @@
 import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    nav {
+        display: flex;
+        gap: 0.75rem;
+        
+        a {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: ${(props) => props.theme.colors['brand-purple-dark']};
+            background: ${(props) => props.theme.colors['brand-purple-light']};
+
+            svg {
+                ${(props) => props.theme.colors['brand-purple']};
+            }
+
+            &:last-child {
+                color: ${(props) => props.theme.colors['brand-yellow-dark']};
+                background: ${(props) => props.theme.colors['brand-yellow-light']};
+            }
+        }
+    }
+`
