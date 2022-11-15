b5a6420744b4b773a5f90a879b84997f09918cc4
WagaoFC <wagnerferreira_07@hotmail.com>
2022-11-15 12:56:24 -0300

    iniciando o desenvolvimento da intro

---
+++ src/pages/Home/components/Intro/index.tsx
@@ -0,0 +1,29 @@
+import { BenefitsContainer, IntroContainer, IntroContent, IntroTitle } from './styles'
+import introImg from '../../../../assets/intro-img.png'
+import { RegularText } from '../../../../components/Typography'
+
+export function Intro() {
+    return (
+        <IntroContainer>
+            <IntroContent className='container'>
+                <div>
+                    <section>
+                        <IntroTitle size='xl'>
+                            Encontre o café perfeito para qualquer hora do dia
+                        </IntroTitle>
+                        <RegularText size='l' color='subtitle' as='h3'>
+                            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
+                        </RegularText>
+                    </section>
+                    <BenefitsContainer>
+                        <p>teste</p>
+                        <p>teste</p>
+                        <p>teste</p>
+                        <p>teste</p>
+                    </BenefitsContainer>
+                </div>
+                <img src={introImg} />
+            </IntroContent>
+        </IntroContainer>
+    )
+}
